18877ce609c67af762b97b600a9323548a37d47d
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2023-08-11 13:16:14 +0600

    complete Promotional Mail

--- resources/views/email/CampaignMail.blade.php
+++ resources/views/email/CampaignMail.blade.php
@@ -1,12 +1,34 @@
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $offerSub }}</title>
 </head>
+<body style="font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f6f6f6;">
 
-<body>
-<p>{{ $offerBody }}</p>
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;">
+    <tr>
+        <td align="center" bgcolor="#ffffff" style="padding: 40px 0;">
+            <img src="https://example.com/logo.png" alt="Logo" width="150" style="display: block;">
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="#ffffff" style="padding: 40px 30px;">
+            <h1 style="font-size: 24px; margin: 0;">Introducing Our Exquisite New Arrivals</h1>
+            <p style="font-size: 16px; margin: 20px 0;">{{ $offerBody }}</p>
+            <a href="https://example.com/shop" style="display: inline-block; padding: 10px 20px; background-color: #ff6600; color: #ffffff; text-decoration: none; border-radius: 5px; font-weight: bold;">Shop Now</a>
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="#ffffff" align="center" style="padding: 40px 0;">
+            <p style="font-size: 16px; margin: 0;">Thank you for being a part of the [Your Company Name] family. Your support fuels our passion for creating beauty and quality that lasts. We look forward to serving you with our latest collection.</p>
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="#f6f6f6" align="center" style="padding: 20px 0;">
+            <p style="font-size: 14px; margin: 0;">[Unsubscribe Link] | [Privacy Policy Link]</p>
+        </td>
+    </tr>
+</table>
+
 </body>
-
 </html>
